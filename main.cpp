dbb5bb5aae116e6a6d05f03148a06affe817132b
Zara28 <31924139+Zara28@users.noreply.github.com>
2017-09-16 16:15:56 +0300

    Начинаю добавлять команды

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 
 using namespace std;
 
+void Real ();
+void Bavaria ();
+void Volga ();
 
 int main()
 {
-    txCreateWindow ( 1000, 600);
+    txCreateWindow ( 1250, 600);
 
     char komanda[20];
 
@@ -24,6 +27,7 @@ int main()
     if (strcmp(komanda, "1") == 0)
     {
         txTextOut (50, 550, "Real Madrid");
+        Real ();
     }
     else if (strcmp(komanda, "2") == 0)
     {
@@ -34,21 +38,93 @@ int main()
         txTextOut (50, 550, "Volga");
     }
 
-    txCircle(200, 130, 20);
-    txCircle(200, 230, 20);
-    txCircle(100, 300, 20);
-    txCircle(200, 430, 20);
-    txCircle(300, 300, 20);
-    txCircle(400, 350, 20);
-    txCircle(400, 200, 20);
-    txCircle(450, 400, 20);
-    txCircle(410, 250, 20);
-    txCircle(310, 460, 20);
+
 
     txDeleteDC (FootballPole);
 
     return 0;
 }
 
+    void Real ()
+    {
+    txSelectFont ("Arial", 20, 0, FW_BOLD);
 
+    txCircle (200, 130, 20);
+    txCircle (200, 230, 20);
+    txCircle (100, 300, 20);
+    txCircle (200, 430, 20);
+    txCircle (300, 300, 20);
+    txCircle (400, 350, 20);
+    txCircle (400, 200, 20);
+    txCircle (450, 400, 20);
+    txCircle (410, 250, 20);
+    txCircle (310, 460, 20);
 
+    txTextOut  (1000, 100, "Ronaldo");
+    txTextOut  (1000, 150, "Benzema");
+    txTextOut  (1000, 200, "Bale");
+    txTextOut  (1000, 250, "Mondric");
+    txTextOut  (1000, 300, "Kroos");
+    txTextOut  (1000, 350, "Casemiro");
+    txTextOut  (1000, 400, "Marselo");
+    txTextOut  (1000, 450, "Carvajai");
+    txTextOut  (1000, 500, "Ramos");
+    txTextOut  (1000, 550, "Pepe");
+    txTextOut  (1000, 575, "Mavus");
+    }
+
+    void Bavaria ()
+    {
+    txSelectFont ("Arial", 20, 0, FW_BOLD);
+
+    txCircle (200, 130, 20);
+    txCircle (200, 230, 20);
+    txCircle (100, 300, 20);
+    txCircle (200, 430, 20);
+    txCircle (300, 300, 20);
+    txCircle (400, 350, 20);
+    txCircle (400, 200, 20);
+    txCircle (450, 400, 20);
+    txCircle (410, 250, 20);
+    txCircle (310, 460, 20);
+
+    txTextOut  (1000, 100, "Ronaldo");
+    txTextOut  (1000, 150, "Benzema");
+    txTextOut  (1000, 200, "Bale");
+    txTextOut  (1000, 250, "Mondric");
+    txTextOut  (1000, 300, "Kroos");
+    txTextOut  (1000, 350, "Casemiro");
+    txTextOut  (1000, 400, "Marselo");
+    txTextOut  (1000, 450, "Carvajai");
+    txTextOut  (1000, 500, "Ramos");
+    txTextOut  (1000, 550, "Pepe");
+    txTextOut  (1000, 575, "Mavus");
+    }
+
+    void Volga ()
+    {
+    txSelectFont ("Arial", 20, 0, FW_BOLD);
+
+    txCircle (200, 130, 20);
+    txCircle (200, 230, 20);
+    txCircle (100, 300, 20);
+    txCircle (200, 430, 20);
+    txCircle (300, 300, 20);
+    txCircle (400, 350, 20);
+    txCircle (400, 200, 20);
+    txCircle (450, 400, 20);
+    txCircle (410, 250, 20);
+    txCircle (310, 460, 20);
+
+    txTextOut  (1000, 100, "Ronaldo");
+    txTextOut  (1000, 150, "Benzema");
+    txTextOut  (1000, 200, "Bale");
+    txTextOut  (1000, 250, "Mondric");
+    txTextOut  (1000, 300, "Kroos");
+    txTextOut  (1000, 350, "Casemiro");
+    txTextOut  (1000, 400, "Marselo");
+    txTextOut  (1000, 450, "Carvajai");
+    txTextOut  (1000, 500, "Ramos");
+    txTextOut  (1000, 550, "Pepe");
+    txTextOut  (1000, 575, "Mavus");
+    }
