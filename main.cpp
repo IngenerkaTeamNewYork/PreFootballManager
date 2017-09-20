0c610abae4281c2a4acbb33a42a6a1dad260581f
Zara28 <31924139+Zara28@users.noreply.github.com>
2017-09-20 14:55:01 +0300

    Вторая часть
    Добавились составы команд, команд стало две

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <TXLib.h>
+#include "TXLib.h"
 
 using namespace std;
 
 void Real ();
-void Bavaria ();
+void BayernMunich ();
 void Volga ();
+void Circle1 ();
+void Circle2 ();
 
 int main()
 {
@@ -31,11 +33,13 @@ int main()
     }
     else if (strcmp(komanda, "2") == 0)
     {
-        txTextOut (50, 550, "Bayern Munich");
+        txTextOut (50, 550, "Bayren Munich");
+        BayernMunich ();
     }
     else if (strcmp(komanda, "3") == 0)
     {
         txTextOut (50, 550, "Volga");
+        Volga ();
     }
 
 
@@ -45,20 +49,42 @@ int main()
     return 0;
 }
 
+    void Circle1 ()
+    {
+    txCircle (200, 130, 20);
+    txCircle (200, 200, 20);
+    txCircle (125, 300, 20);
+    txCircle (200, 430, 20);
+    txCircle (300, 385, 20);
+    txCircle (385, 350, 20);
+    txCircle (400, 200, 20);
+    txCircle (450, 352, 20);
+    txCircle (480, 250, 20);
+    txCircle (310, 460, 20);
+    }
+
+    void Circle2 ()
+    {
+    txSetColor (TX_BLUE);
+    txSetFillColor (TX_BLUE);
+
+    txCircle (800, 130, 20);
+    txCircle (800, 230, 20);
+    txCircle (900, 300, 20);
+    txCircle (800, 430, 20);
+    txCircle (700, 300, 20);
+    txCircle (600, 350, 20);
+    txCircle (600, 200, 20);
+    txCircle (550, 400, 20);
+    txCircle (590, 250, 20);
+    txCircle (690, 460, 20);
+    }
+
     void Real ()
     {
     txSelectFont ("Arial", 20, 0, FW_BOLD);
 
-    txCircle (200, 130, 20);
-    txCircle (200, 230, 20);
-    txCircle (100, 300, 20);
-    txCircle (200, 430, 20);
-    txCircle (300, 300, 20);
-    txCircle (400, 350, 20);
-    txCircle (400, 200, 20);
-    txCircle (450, 400, 20);
-    txCircle (410, 250, 20);
-    txCircle (310, 460, 20);
+    Circle1 ();
 
     txTextOut  (1000, 100, "Ronaldo");
     txTextOut  (1000, 150, "Benzema");
@@ -71,60 +97,58 @@ int main()
     txTextOut  (1000, 500, "Ramos");
     txTextOut  (1000, 550, "Pepe");
     txTextOut  (1000, 575, "Mavus");
+
+
     }
 
-    void Bavaria ()
+    void BayernMunich ()
     {
     txSelectFont ("Arial", 20, 0, FW_BOLD);
+    txSetColor (TX_YELLOW);
+    txSetFillColor (TX_YELLOW);
 
-    txCircle (200, 130, 20);
-    txCircle (200, 230, 20);
-    txCircle (100, 300, 20);
-    txCircle (200, 430, 20);
-    txCircle (300, 300, 20);
-    txCircle (400, 350, 20);
-    txCircle (400, 200, 20);
-    txCircle (450, 400, 20);
-    txCircle (410, 250, 20);
-    txCircle (310, 460, 20);
+    Circle1 ();
 
-    txTextOut  (1000, 100, "Ronaldo");
-    txTextOut  (1000, 150, "Benzema");
-    txTextOut  (1000, 200, "Bale");
-    txTextOut  (1000, 250, "Mondric");
-    txTextOut  (1000, 300, "Kroos");
-    txTextOut  (1000, 350, "Casemiro");
-    txTextOut  (1000, 400, "Marselo");
-    txTextOut  (1000, 450, "Carvajai");
-    txTextOut  (1000, 500, "Ramos");
-    txTextOut  (1000, 550, "Pepe");
-    txTextOut  (1000, 575, "Mavus");
+    txTextOut  (1000, 100, "Lewandowski");
+    txTextOut  (1000, 150, "Riberg");
+    txTextOut  (1000, 200, "Muller");
+    txTextOut  (1000, 250, "Robben");
+    txTextOut  (1000, 300, "Thiago");
+    txTextOut  (1000, 350, "Kroos");
+    txTextOut  (1000, 400, "Alaba");
+    txTextOut  (1000, 450, "Kimmicn");
+    txTextOut  (1000, 500, "Boateng");
+    txTextOut  (1000, 550, "Hummeis");
+    txTextOut  (1000, 575, "Neuer");
+
+    txSetColor (TX_BLUE);
+    txSetFillColor (TX_BLUE);
+
+    Circle2 ();
     }
 
     void Volga ()
     {
     txSelectFont ("Arial", 20, 0, FW_BOLD);
+    txSetColor (TX_YELLOW);
+    txSetFillColor (TX_YELLOW);
 
-    txCircle (200, 130, 20);
-    txCircle (200, 230, 20);
-    txCircle (100, 300, 20);
-    txCircle (200, 430, 20);
-    txCircle (300, 300, 20);
-    txCircle (400, 350, 20);
-    txCircle (400, 200, 20);
-    txCircle (450, 400, 20);
-    txCircle (410, 250, 20);
-    txCircle (310, 460, 20);
+    Circle1();
 
-    txTextOut  (1000, 100, "Ronaldo");
-    txTextOut  (1000, 150, "Benzema");
-    txTextOut  (1000, 200, "Bale");
-    txTextOut  (1000, 250, "Mondric");
-    txTextOut  (1000, 300, "Kroos");
-    txTextOut  (1000, 350, "Casemiro");
-    txTextOut  (1000, 400, "Marselo");
-    txTextOut  (1000, 450, "Carvajai");
-    txTextOut  (1000, 500, "Ramos");
-    txTextOut  (1000, 550, "Pepe");
-    txTextOut  (1000, 575, "Mavus");
+    txTextOut  (1000, 100, "Нигматуллин Артур");
+    txTextOut  (1000, 150, "Пасько Артём");
+    txTextOut  (1000, 200, "Полянин Дмитрий");
+    txTextOut  (1000, 250, "Маляка Дмитрий");
+    txTextOut  (1000, 300, "Костюков Михаил");
+    txTextOut  (1000, 350, "Шеляков Алексей");
+    txTextOut  (1000, 400, "Леандро да Силва");
+    txTextOut  (1000, 450, "Петров Илья");
+    txTextOut  (1000, 500, "Саркисов Артур");
+    txTextOut  (1000, 550, "Кухарчук Илья");
+    txTextOut  (1000, 575, "Вдовиченко Николай");
+
+    txSetColor (TX_BLUE);
+    txSetFillColor (TX_BLUE);
+
+   Circle2 ();
     }
